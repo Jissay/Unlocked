1ed2b2b82a96b76b12c360dd6f994698ce6b7935
Jissay <jc.ricard@me.com>
2019-12-08 22:04:27 +0100

    Created SERVER_APP folder and bootable setup

---
+++ SERVER_APP/src/test/kotlin/com/jissay/unlocked/UnlockedServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jissay.unlocked
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UnlockedServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
